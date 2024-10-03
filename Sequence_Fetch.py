4a3b12b67fdfd7e2ff10022edf1463f37a4c02ff
Eli Bring Horvath <97261650+ERBringHorvath@users.noreply.github.com>
2024-10-03 15:13:55 -0700

    Update Sequence_Fetch.py

--- Sequence_Fetch.py
+++ Sequence_Fetch.py
@@ -1,6 +1,11 @@
 import pandas as pd
 from Bio import SeqIO
 
+"""
+Extract specified sequences from a multi-FASTA file based on sequence header information 
+in a curated CSV file. 
+"""
+
 # read the CSV file into a pandas DataFrame
 df = pd.read_csv('file.csv')
 
